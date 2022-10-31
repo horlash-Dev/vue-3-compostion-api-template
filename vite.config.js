01419cbc553295e79f00a008920e0822b842a2ce
Abdulqawiyyi abiodun <86069758+horlash-Dev@users.noreply.github.com>
2022-10-31 13:16:50 +0100

    firebase config  added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 const root = resolve(__dirname, "src");
-//const outDir = resolve(__dirname,"dist")
+const outDir = resolve(__dirname, "dist");
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
@@ -15,11 +15,13 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: outDir,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(root, "index.html"),
-        login: resolve(root, "src", "login", "index.html"),
-        register: resolve(root, "src", "register", "index.html"),
+        login: resolve(root, "login", "index.html"),
+        register: resolve(root, "register", "index.html"),
       },
     },
   },
